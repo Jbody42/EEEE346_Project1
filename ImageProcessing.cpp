c4850befe1483655e0c837460464a9fb2395e013
jbody42 <jbody121@gmail.com>
2020-09-27 13:33:41 -0400

    copy function made

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
-
+#include "PGM.h"
 #include "ImageProcessing.h"
+
+
+void copyfile(string filename){
+    
+
+    if (openPGM(filename)){
+        int width = getPGMWidth();
+        int height = getPGMHeight();
+
+        int** original;
+        original = new int*[height];
+        
+        for (int i = 0; i < height; i++){
+            original[i] = new int [width];
+        }
+
+        getPGMData(original);
+
+        writePGM("copy.pgm", original);
+
+        for(int i = 0; i < height; i++){
+            delete[] original[i];
+        }
+        delete original;
+    }
+}
+
 
 
