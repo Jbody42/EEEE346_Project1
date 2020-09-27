95185e5344ded2e99d4d5cb0b59a054cd4cc184d
Will Wright <williamwright5191@gmail.com>
2020-09-27 13:43:27 -0400

    creating & testing functions

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -1,6 +1,51 @@
-#include <iostream>
-using namespace std;
-
 #include "ImageProcessing.h"
 
 
+
+void hFlip(int** data){
+    /*string fileName = "ballons.pgm";
+    
+    //Open File set information
+    if( openPGM(fileName) )
+    {
+        // Get Image Size Information
+        int width = getPGMWidth();
+        int height = getPGMHeight();
+        // Declare and allocate memory for data
+        int** data;
+        data = new int*[ height];
+
+        for(int i = 0; i < height; i++){
+            data[i] = new int[width];
+        }
+
+        // Get the data
+        getPGMData(data);
+
+        // Write back out a horizontally-flipped image
+        for(int i = 0; i < height; i++){
+            data[i] = flip_array(data, width);
+        }
+        
+        // Clean up memory
+        for(int i = 0; i < height; i++){
+            delete[] data[i];
+        }
+        delete data;
+    }
+    */
+}
+
+
+void flip_array(int array[], int n){
+
+    int hold[n];
+
+    for(size_t i = 0; i < n-1; i++){
+        hold[n-i] = array[i];
+    }
+
+    for(size_t i = 0; i < n; i++){
+        array[i] = hold [i];
+    }
+}
--- ImageProcessing.h
+++ ImageProcessing.h
@@ -1,4 +1,16 @@
 #include <iostream>
-using namespace std;
+#include "PGM.h"
 #include <stdio.h>
 #include <string.h>
+using namespace std;
+
+/*
+* Function to horizontally flip an image
+* and output the image file
+*/
+void hFlip(int data[], int w, int h );
+
+/*
+* Function to flip an array
+*/
+void flip_array(int array[], int n);
