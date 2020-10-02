0466c50b520d4ca012a149d418e19512d7c6c94a
jbody42 <jbody121@gmail.com>
2020-10-02 11:52:09 -0400

    rewrote the main function with a while loop instead of recursion

--- main.cpp
+++ main.cpp
@@ -1,59 +1,65 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 #include "PGM.h"
 using namespace std;
 #include "ImageProcessing.h"
 
 int main(){
-    string filename, answer, newname;
+    string file, answer, newname;
     int x;
-    cin >> filename;
+    answer = 'y';
+    while(answer.compare("y") == 0){
+        cout << "Enter Original File Name: ";
+        cin >> file;
+        cout << "Select Operation:" << endl;
+        cout << "       (0) Copy Image" << endl;
+        cout << "       (1) Flip Vertical" << endl;
+        cout << "       (2) Flip Horizontal" << endl;
+        cout << "       (3) Median Filter" << endl;
+        cout << "Enter Selection: ";
+        cin >> x;
 
-    cout << "Select Operation:";
-    cout << "       (0) Copy Image";
-    cout << "       (1) Flip Vertical";
-    cout << "       (2) Flip Horizontal";
-    cout << "       (3) Median Filter";
-    cout << "Enter Selection:  ";
-    cin >> x;
-    if (x == 0){
-        cout << "Enter Save File Name: ";
-        cin >> newname;
-        copyImage(filename, newname);
+   
+        if (x == 0){
+            cout << "Enter Save File Name: ";
+            cin >> newname;
+            copyImage(file, newname);
+            cout << "Perform another operation [y/n]? ";
+            cin >> answer;
+        }
+
+        else if (x == 1){
+            cout << "Enter Save File Name:";
+            cin >> newname;
+
+            cout << "Perform another operation [y/n]?";
+            cin >> answer;
+        }
+
+        else if (x == 2){
+            cout << "Enter Save File Name:";
+            cin >> newname;
+
+            cout << "Perform another operation [y/n]?";
+            cin >> answer;
+        }
+
+        else if (x == 3){
+            cout << "Enter Save File Name:";
+            cin >> newname;
+                
+            cout << "Perform another operation [y/n]?";
+            cin >> answer;
+
+        }
+
+        else{
+            cout << "Please enter a valid number";
+
+        }
+
 
     }
-
-    else if (x == 1){
-        cout << "Enter Save File Name: ";
-        cin >> newname;
-
-    }
-
-    else if (x == 2){
-        cout << "Enter Save File Name: ";
-        cin >> newname;
-
-    }
-
-    else if (x == 3){
-        cout << "Enter Save File Name: ";
-        cin >> newname;
-
-    }
-
-    else{
-
-
-    }
-
-    cout << "Perform another operation [y/n]?";
-    cin >> answer;
-
-    if (answer.compare("y") == 0){
-        main();
-    }
-
-    else return 0;
 
     return 1;
 }
